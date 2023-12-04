49f1e38bfb193a8eede472a4fb97b7908e155601
JuanCarlos11902 <105056958+JuanCarlos11902@users.noreply.github.com>
2023-12-04 09:59:13 +0100

    terminado el back

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ const express = require('express');
 const app = express()
 
 app.get('/obtener-preguntas-generales', async(req,res) =>{
+ try {
+        const respuesta = await axios.get('https://opentdb.com/api.php?amount=20');
+        let arrayDevuelto = [];
 
-    const respuesta = await axios.get('');
+        for (let i = 0; i < respuesta.data.results.length; i++) {
+            let object = {
+                difficulty: respuesta.data.results[i].difficulty,
+                question: respuesta.data.results[i].question,
+                correctAnswer: respuesta.data.results[i].correct_answer,
+                incorrectAnswer: respuesta.data.results[i].incorrect_answers,
+            };
 
+            arrayDevuelto.push(object);
+        }
+
+        res.json(arrayDevuelto);
+    } catch (error) {
+        console.error("Error:", error.message);
+        res.status(500).send('Error interno del servidor');
+    }
+    
+
+})
+
+    app.listen(5000, () =>{
+    console.log("Estamos en directo chicos")
 })
